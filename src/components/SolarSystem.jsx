1c9adf8e783ea30c90020a6a76f13ad01bdd3184
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-21 15:42:41 -0300

    add renderPlanets function - Requirement 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Planets from '../data/planets';
 import PlanetCard from './PlanetCard';
 import Title from './Title';
+
+function renderPlanets() {
+  return Planets.map((planet) => (
+    <PlanetCard
+      planetName={ planet.name }
+      planetImage={ planet.image }
+      key={ planet.name }
+    />
+  ));
+}
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <PlanetCard planetName="TERRA" />
+        {renderPlanets()}
       </div>
     );
   }
