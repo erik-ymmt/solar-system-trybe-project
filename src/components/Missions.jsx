d61966216c01d529c221732e16f76b13f24e1d3b
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-21 16:03:43 -0300

    add renderMission function - Requirement 10

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
+import Missions from '../data/missions';
 import MissionCard from './MissionCard';
 import Title from './Title';
+
+function renderMissions() {
+  return Missions.map((mission) => (
+    <MissionCard
+      name={ mission.name }
+      year={ mission.year }
+      country={ mission.country }
+      destination={ mission.destination }
+      key={ mission.name }
+    />
+  ));
+}
 
 class Mission extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        <MissionCard name="teste" />
+        { renderMissions() }
       </div>
     );
   }
