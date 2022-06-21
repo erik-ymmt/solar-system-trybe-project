b5923fbc6f9d2e7cc74c438fde28c5060be1f87f
Erik Yamamoto <erik.ymmt@gmail.com>
2022-06-21 16:22:42 -0300

    add CSS for Header and App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Mission from './components/Missions';
 import SolarSystem from './components/SolarSystem';
+import './App.css';
 
 class App extends React.Component {
   render() {
